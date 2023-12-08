1d92ed728168830d23180a4a2af775bd92c51808
TejaswaBedi <92619400+TejaswaBedi@users.noreply.github.com>
2023-12-09 00:46:31 +0530

    Update Order.js

--- modal/Order.js
+++ modal/Order.js
@@ -1,19 +1,22 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
-const orderSchema = new Schema({
-  items: { type: [Schema.Types.Mixed], required: true },
-  totalAmount: { type: Number },
-  totalItems: { type: Number },
-  user: {
-    type: Schema.Types.ObjectId,
-    ref: "User",
-    required: true,
+const orderSchema = new Schema(
+  {
+    items: { type: [Schema.Types.Mixed], required: true },
+    totalAmount: { type: Number },
+    totalItems: { type: Number },
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    paymentMethod: { type: String, required: true },
+    status: { type: String, default: "pending" },
+    selectedAddress: { type: Schema.Types.Mixed, required: true },
   },
-  paymentMethod: { type: String, required: true },
-  status: { type: String, default: "pending" },
-  selectedAddress: { type: Schema.Types.Mixed, required: true },
-});
+  { timestamps: true }
+);
 
 const virtual = orderSchema.virtual("id");
 virtual.get(function () {
